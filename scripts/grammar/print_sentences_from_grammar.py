722879be07452829697a7f21567be6ff8980659c
Cécile Macaire <cecile.macaire@univ-grenoble-alpes.fr>
2023-10-18 10:26:33 +0200

    final code with comments

--- scripts/grammar/print_sentences_from_grammar.py
+++ scripts/grammar/print_sentences_from_grammar.py
@@ -1,7 +1,22 @@
+"""
+Methods to show the translation in pictograms in a html file.
+"""
+
 from grammar import *
 
 
 def create_html_file(html_file):
+    """
+        Create the header of the html file.
+
+        Arguments
+        ---------
+        html_file: str
+
+        Returns
+        -------
+        The html file.
+    """
     header = "<!doctype html>" \
              "<html lang=\"fr\"><head>" \
              "<meta charset=\"utf-8\">" \
@@ -19,6 +34,14 @@ def create_html_file(html_file):
 
 
 def write_header_info_per_sentence(html_file, utt_name):
+    """
+        Write the content of the header for each sentence.
+
+        Arguments
+        ---------
+        html_file: File
+        utt_name: str
+    """
     html_file.write("<div class=\"container-fluid\">")
     html_file.write("<div class=\"row\">")
     html_file.write("<div class=\"col-12\"><div class=\"p-3\">")
@@ -27,6 +50,14 @@ def write_header_info_per_sentence(html_file, utt_name):
 
 
 def write_translations_per_sentence(html_file, translations):
+    """
+        Write the the pictogram image in a div.
+
+        Arguments
+        ---------
+        html_file: file
+        translations: list
+    """
     if translations is not None:
         for id_picto in translations:
             if id_picto.to_picto:
@@ -52,15 +83,19 @@ def write_translations_per_sentence(html_file, translations):
                             id_picto.lemma) + "<br/>Pos : " + id_picto.pos + "<br/>WSD : " + str(
                             info_wsd) + "<br/>Id picto : " + str(
                             id_picto.picto) + "</figcaption></figure>")
-                    # "<img src=\"/data/macairec/Cloud/PROPICTO_RESSOURCES/ARASAAC/ARASAAC_Pictos_All/" + str(
-                    #     id_picto.picto[0]) + ".png" + "\"alt=\"\" width=\"150\" height=\"150\" />"
-                    #                                   "<figcaption class=\"figure-caption text-center\">Token : " + id_picto.token + "<br/>Lemma : " + str(
-                    #     id_picto.lemma) + "<br/>Pos : " + id_picto.pos + "<br/>Id picto : " + str(
-                    #     id_picto.picto) + "</figcaption></figure>")
                 html_file.write("&nbsp;&nbsp;")
 
 
 def print_pictograms(sentence, sentence_grammar, html_file):
+    """
+        Create the html file.
+
+        Arguments
+        ---------
+        sentence: list
+        sentence_grammar: list
+        html_file: str
+    """
     html = create_html_file(html_file)
     html.write("<div class = \"container\">")
     for i, s in enumerate(sentence):
